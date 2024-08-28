4f6871db37c4b18a226a09ab3016c4fc0ea6f4ff
Sanvir-Bal <83791711+Sanvir-Bal@users.noreply.github.com>
2024-08-28 00:42:18 -0700

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from datetime import datetime
+
+
+
+# Title of the page
+st.title("⏰ Time Saver Hub")
+
+# Phrase below the clock
+st.header("**Time is always at work.**")
+st.header("**Let's help you catch up.**")
+
+st.divider()
+
+# Descriptions of the apps
+st.subheader("The Apps")
+
+st.markdown("Below you'll find a selection of apps curated to save you time and streamline your tasks. These tools are designed to make your life easier and more efficient.")
+
+st.divider()
+
+st.markdown("**📺 YouTube Summarizer:** Quickly get concise summaries of any YouTube video in 500 words or less.")
+
+st.markdown("**💬 PDF Chat:** Upload PDFs and ask questions to get precise answers, saving you from reading every document.")
+
+st.markdown("**🔍 ATS Check:** Upload your resume and job description to identify missing keywords and refine your resume.")
